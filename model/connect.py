9fbbafe1314ce38a0cc0b4e7c53b2d66f502805f
unknown <saraharyandi@gmail.com>
2025-12-12 20:04:09 +0700

    fix connect2

--- model/connect.py
+++ model/connect.py
@@ -5,7 +5,8 @@ db = mysql.connector.connect(
     host= os.getenv('MYSQLHOST'),        # atau IP server MySQL
     user=os.getenv('MYSQLUSER'),
     password=os.getenv('MYSQLPASSWORD'),
-    database=os.getenv('MYSQLDATABASE')
+    database=os.getenv('MYSQLDATABASE'),
+    port=os.getenv('MYSQLPORT')
 )
 
 cursor = db.cursor()
