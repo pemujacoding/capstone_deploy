d95fcd74f7a43568de975f5e3a68125113f7847c
unknown <saraharyandi@gmail.com>
2025-12-12 21:05:21 +0700

    connect fix

--- model/connect.py
+++ model/connect.py
@@ -3,10 +3,10 @@ import os
 
 db = mysql.connector.connect(
     host= "yamanote.proxy.rlwy.net",        # atau IP server MySQL
-    user=os.getenv('MYSQLUSER'),
-    password=os.getenv('MYSQLPASSWORD'),
-    database=os.getenv('MYSQLDATABASE'),
-    port = 58251
+    user="root",
+    password="vdUUleETGAYacyXsNJJRTZcqNIEKDWoS",
+    database="railway",
+    port = "58251"
 )
 
 cursor = db.cursor()
