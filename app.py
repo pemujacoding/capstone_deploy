70cf65fa46ae4df0279c7e9f20e5cb93a56e68f0
unknown <saraharyandi@gmail.com>
2025-12-12 20:55:44 +0700

    fix app

--- app.py
+++ app.py
@@ -400,6 +400,6 @@ def reanalyze_gemini(interview_id):
     
     return redirect(url_for('history'))
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
