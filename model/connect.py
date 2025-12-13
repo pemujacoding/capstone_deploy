eba44d5ac342684cb36647cafad6461555e454e7
unknown <saraharyandi@gmail.com>
2025-12-13 09:23:35 +0700

    change root pass

--- model/connect.py
+++ model/connect.py
@@ -1,8 +1,7 @@
 import mysql.connector
 from decouple import config
 
-passs = config("MYSQL_PASSWORD")
-print (passs)
+passs = config("MYSQL_ROOT_PASSWORD")
 db = mysql.connector.connect(
     host= "yamanote.proxy.rlwy.net",        # atau IP server MySQL
     user="root",
