54c3f80c304dd4337377119f5a58d42d58198e36
unknown <saraharyandi@gmail.com>
2025-12-13 09:37:11 +0700

    change conn

--- model/connect.py
+++ model/connect.py
@@ -1,30 +1,16 @@
 import mysql.connector
 from decouple import config
+import os
 
- # Ambil environment variable
-db_user = config("MYSQLUSER")
-db_pass = config("MYSQLPASSWORD")
-db_name = config("MYSQLDATABASE")
-
-# Validasi kalau ada yang belum di-set
-missing = [k for k,v in {
-    "MYSQLHOST": "yamanote.proxy.rlwy.net",
-    "MYSQLPORT": 58251,
-    "MYSQLUSER": db_user,
-    "MYSQLPASSWORD": db_pass,
-    "MYSQLDATABASE": db_name
-}.items() if not v]
-
-if missing:
-    raise RuntimeError(f"Environment variable(s) missing: {', '.join(missing)}")
-
-# Connect ke DB
+passs = config("MYSQL_PASSWORD")
+print (passs)
 db = mysql.connector.connect(
-    host="yamanote.proxy.rlwy.net",
-    port= 58251,
-    user=db_user,
-    password=db_pass,
-    database=db_name
+    host= "yamanote.proxy.rlwy.net",        # atau IP server MySQL
+    user="root",
+    password=os.getenv("MYSQL_PASSWORD"),
+    database= "railway",
+    port = "58251"
 )
 
-cursor = db.cursor
+cursor = db.cursor()
+print("Connected!")
